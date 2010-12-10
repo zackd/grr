d719a6303f4a6cbc3a8a71723950dc498bb1669f
zack <zackdykes@gmail.com>
2010-12-10 16:27:37 +0100

    add book.review, BootStrap textile example doc -> book.review

--- grails-app/domain/org/example/Book.groovy
+++ grails-app/domain/org/example/Book.groovy
@@ -3,8 +3,13 @@ package org.example
 class Book {
 	String title
 	String author
+	String review
+	
     static constraints = {
 		title(blank: false)
 		author(blank: false)
     }
+	static mapping = {
+		review type:'text'
+	}
 }
